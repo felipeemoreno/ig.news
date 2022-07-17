7d9a6b4ea22c33e911493c958ceadf76f582a964
Felipe E. Moreno <contato@wr5solutions.com.br>
2022-07-17 15:06:15 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+     <main>
+      <section>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications<br/>
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding" />
+     </main>
+    </>
+  )
+}
